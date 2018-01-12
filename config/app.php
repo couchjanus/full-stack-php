4f55ef3203f1d3d43b7094b74bd992dbe40de100
Janus Nicon <janusnic@gmail.com>
2018-01-12 13:03:06 +0200

    added unit 4

--- config/app.php
+++ config/app.php
@@ -5,7 +5,12 @@
     define('CONTROLLERS', ROOT.'/controllers/');
     define('CONFIG', ROOT.'/config/');
     define('CORE', ROOT.'/core/');
-
+    define('DB', ROOT.'/db/');
     define('EXT', '.php');
     define('APPNAME', 'Great Shopaholic');
     define('SLOGAN', 'Lets Build Cool Site');
+    
+    define('HOST', 'localhost'); // адрес сервера 
+    define('DATABASE', 'mydb'); // имя базы данных
+    define('DBUSER', 'dev'); // имя пользователя
+    define('DBPASSWORD', 'ghbdtn'); // пароль
