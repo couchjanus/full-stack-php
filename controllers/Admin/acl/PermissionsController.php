70cc944563ef4aaed98195760f09af64c1b0face
couchjanus <couchjanus@gmail.com>
2018-01-31 20:24:35 +0200

    Update PermissionsController.php

--- controllers/Admin/acl/PermissionsController.php
+++ controllers/Admin/acl/PermissionsController.php
@@ -14,7 +14,7 @@ class PermissionsController extends Controller{
         extract($vars);
 
         if (isset($_POST['submit'])) {
-            Permission::delete($id);
+            Permission::destroy($id);
             header('Location: /admin/permissions');
         }
         $data['title'] = 'Admin Permission Delete Page ';
@@ -29,31 +29,31 @@ class PermissionsController extends Controller{
         if (isset($_POST) and !empty($_POST)) {
             $options['name'] = trim(strip_tags($_POST['name']));
 
-            Permission::add($options);
+            Permission::store($options);
             header('Location: /admin/permissions');
         }
         $data['title'] = 'Admin Permission Add Page ';
-       
+
         $this->_view->render('admin/permissions/add',$data);
-        
+
     }
 
     public function edit ($vars) {
-       
+
         extract($vars);
 
         $permission = Permission::get($id);
 
         if (isset($_POST) and !empty($_POST)) {
             $options['name'] = trim(strip_tags($_POST['name']));
-     
-            Permission::edit($id, $options);
+
+            Permission::update($id, $options);
 
             header('Location: /admin/permissions');
         }
         $data['title'] = 'Admin Permission Edit Page ';
         $data['permission'] = $permission;
-        
+
         $this->_view->render('admin/permissions/edit',$data);
 
     }
