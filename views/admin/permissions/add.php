99e6223140df7e3e66f0846cb501f78d2fca2e49
couchjanus <couchjanus@gmail.com>
2018-01-31 20:22:34 +0200

    Update add.php

--- views/admin/permissions/add.php
+++ views/admin/permissions/add.php
@@ -1,19 +1,45 @@
 <?php
-include_once VIEWS.'/shared/admin/header.php';
+include_once VIEWS.'shared/admin/header.php';
 ?>
-        <main>
-            <h1><?= $title;?></h1>
-        </main>
+<div class="page-content">
+   <div class="row">
+        <div class="col-md-2">
+        <?php
+          include_once VIEWS.'shared/admin/_aside.php';
+        ?>
+        </div>
+      <div class="col-md-10">
+        <div class="content-box-large">
+          <div class="panel-heading">
+                <div class="panel-title"><?= $title;?></div>
 
-<article class='large'>
-        <h1>Добавить новню permission</h1>
-        <form action="#" method="post">
-            <p>Название permission</p>
-            <input required type="text" name="name">
-            <input type=submit name="submit" value="Сохранить">
-        </form>
-</article>
+                <div class="panel-options">
+                    <a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>
+                    <a href="#" data-rel="reload"><i class="glyphicon glyphicon-cog"></i></a>
+                </div>
+          </div>
+          <form class="form-horizontal" role="form" id="idForm" method="POST">
+
+            <div class="panel-body">
+
+                <div class="form-group">
+                        <label for="name" class="col-sm-2 control-label"> Name</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="name" name="name" placeholder="Categoty Name">
+                        </div>
+                </div>
+
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                  <button id="save" type="submit" class="save btn btn-primary">Add Permission</button>
+                </div>
+            </div>
+          </form>
+        </div>
+      </div>
+  </div>
+</div>
 
 <?php
-include_once VIEWS.'/shared/admin/footer.php';
-
+include_once VIEWS.'shared/admin/footer.php';
