886b1fb0814367afe4986d29864768dc61651230
janus <couchjanus@gmail.com>
2018-02-02 16:46:10 +0200

    added edit

--- controllers/Admin/shop/ProductsController.php
+++ controllers/Admin/shop/ProductsController.php
@@ -103,4 +103,76 @@ class ProductsController extends Controller {
         $this->_view->render('admin/products/create',$data);
     }
 
+    /**
+     * Удаление конкретного товара
+     *
+     * @param $id товара
+     * @return bool
+     */
+    public function delete ($vars) {
+
+        extract($vars);
+
+        //Проверяем форму
+        if (isset($_POST['submit'])) {
+            //Если отправлена, то удаляем нужный товар
+            Product::delete($id);
+            //и перенаправляем на страницу товаров
+            header('Location: /admin/products');
+        }
+
+        $data['product_id'] = $id;
+        $data['title'] = 'Admin Product Delete Page ';
+        
+        $this->_view->render('admin/products/delete',$data);
+        
+    }
+
+    /**
+     * Редатирование товара
+     *
+     * @param $id
+     * @return bool
+     */
+    public function edit ($vars) {
+
+        //Получаем информацию о выбранном товаре
+        extract($vars);
+
+        $product = Product::getProductById($id);
+
+        //Принимаем данные из формы
+        if (isset($_POST) and !empty($_POST)) {
+            $options['name'] = trim(strip_tags($_POST['name']));
+            
+            $options['price'] = trim(strip_tags($_POST['price']));
+            $options['category'] = trim(strip_tags($_POST['category']));
+            $options['brand'] = trim(strip_tags($_POST['brand']));
+            $options['description'] = trim(strip_tags($_POST['description']));
+            
+            $options['is_new'] = trim(strip_tags($_POST['is_new']));
+            $options['status'] = trim(strip_tags($_POST['status']));
+            
+            // Проверим, загружалось ли через форму изображение
+            if (is_uploaded_file($_FILES["image"]["tmp_name"])) {
+                $file_name = $_FILES['image']['name'];
+                // Если загружалось, переместим его в нужную папке, дадим новое имя
+                move_uploaded_file($_FILES["image"]["tmp_name"], $_SERVER['DOCUMENT_ROOT'] . "/media/products/{$id}/{$file_name}");
+                $options['picture'] = $file_name;
+            }else{
+                $options['picture'] = $product['picture'];
+            }
+            Product::update($id, $options);
+            header('Location: /admin/products');
+        }
+      
+        $data['product'] = Product::getProductById($id);
+        $data['categories'] = Category::index();
+        $data['title'] = 'Admin Product Edit Page ';
+        
+        $this->_view->render('admin/products/edit',$data);
+        
+    }
+
+
 }
